794308821565e78f057b1aa7bbcb822b0eadd8c2
RaulBgm <74044182+RaulBgm@users.noreply.github.com>
2020-12-15 09:56:34 +0100

    Añadida llamada controlador comentario y envio de datos comentarios a vista producto .

--- app/Controllers/Producto.php
+++ app/Controllers/Producto.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers;
 
+use App\Controllers\Comentario;
 use App\Models\ProductoModel;
 use App\Models\ValoracionModel;
 use CodeIgniter\Controller;
@@ -8,17 +9,20 @@ class Producto extends BaseController{
 
     private $model;
     private $vmodel;
+    private $c_controller;
 
     public function __construct()
     {
         $this->model = new ProductoModel();
         $this->vmodel = new ValoracionModel();
+        $this->c_controller = new Comentario();
     }
 
     public function view($productoId = NULL){
         $data= [
-            'producto' => $this->model->getProducto($productoId),
-            'valoracion' => $this->vmodel
+            'producto'   => $this->model->getProducto($productoId),
+            'valoracion' => $this->vmodel,
+            'comentario' => $this->c_controller->obtenerComentariosProducto($productoId)
         ];
 
         if(empty($data['producto'])){
