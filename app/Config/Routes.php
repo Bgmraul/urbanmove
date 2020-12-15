af4b3559280363c2c0115c80b7e2449f127b6cd2
RaulBgm <74044182+RaulBgm@users.noreply.github.com>
2020-12-15 09:54:40 +0100

    Añadida ruta para crear comentarios.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->match(['get', 'post'], 'users/create', 'Usuario::create');
 $routes->match(['get', 'post'], '/valoracion', 'Valoracion::insertarValoracion');
+$routes->match(['get', 'post'], '/comentario/guardarComentarios', 'Comentario::guardarComentarios');
 $routes->get('valoracion/datos/(:segment)', 'Valoracion::obtenerValoracion/$1');
 $routes->get('users/(:segment)', 'Usuario::view/$1');
 $routes->get('usuario', 'Usuario::index');
